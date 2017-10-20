e072ce7bce2eebd77fe92afb20791a99a5743be3
arthur.yueh <arthur.yueh@vpon.com>
2017-10-20 10:38:00 +0800

    add sed port so that nagios can use --net=host can use the host's consul dns

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -14,6 +14,8 @@ rsync -av --ignore-existing /var/opt/nagios/etc /usr/local/nagios
 # Sync Nagios var directory from working to original directory
 rsync -av --ignore-existing /var/opt/nagios/var /usr/local/nagios
 
+sed -i "s/80/${NAGIOS_PORT:-80}/g" /etc/apache2/sites-available/000-default.conf /etc/apache2/ports.conf
+
 # Restart the Nagios Service
 service nagios restart
 
